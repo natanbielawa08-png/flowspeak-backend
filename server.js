36b76d822fa7843760876e3c7da25bfbe5f8e0b2
natanbielawa08-png <natanbielawa08@gmail.com>
2026-05-28 21:38:45 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-cd C:\Windows\System32\flowspeak-backend
-git init
-git add server.js
-git commit -m "Add server.js"
-git remote add origin https://github.com/natanbielawa08-png/flowspeak-backend.git
-git push -u origin main
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('FlowSpeak backend is running!');
+});
+
+app.post('/retell-webhook', (req, res) => {
+    console.log('Received webhook from Retell:');
+    console.log('Body:', req.body);
+    res.status(200).json({ received: true });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Retell webhook endpoint: http://localhost:${PORT}/retell-webhook`);
+});
