0ef74f03e2c1305e31d1d529b04c4a21abe3d6c0
natanbielawa08-png <natanbielawa08@gmail.com>
2026-05-28 21:17:36 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+cd C:\Windows\System32\flowspeak-backend
+git init
+git add server.js
+git commit -m "Add server.js"
+git remote add origin https://github.com/natanbielawa08-png/flowspeak-backend.git
+git push -u origin main
